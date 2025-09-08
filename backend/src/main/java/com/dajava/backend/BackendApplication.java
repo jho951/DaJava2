e5f3afcad9f85bec47613c7cf69bf923f96ca2b7
jhons <jho951@naver.com>
2025-09-08 15:11:15 +0900

    refactor: 프로젝트 멀티 모듈 설계로 변경

--- backend/src/main/java/com/dajava/backend/BackendApplication.java
+++ backend/src/main/java/com/dajava/backend/BackendApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import lombok.extern.slf4j.Slf4j;
 
 @EnableAsync
 @EnableScheduling
@@ -16,7 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 @ServletComponentScan
 @EnableAspectJAutoProxy
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
